f12c230fdcbb269c0cdae4759059f70ce88dbac2
clan0304 <clan0304@gmail.com>
2024-01-15 23:48:07 +1100

    changed

--- app/page.js
+++ app/page.js
@@ -8,7 +8,7 @@ import MainSentence from '@/components/MainSentence';
 export default function Home() {
   return (
     <main>
-      <div className="relative aspect-video w-full">
+      <div className="relative aspect-[4/3] w-full">
         <div className="flex flex-col absolute w-full top-0 left-0 z-20">
           <Navbar />
         </div>
@@ -23,8 +23,8 @@ export default function Home() {
           />
         </div>
       </div>
-      <div className="flex flex-col sm:flex-row w-full sm:aspect-video">
-        <div className="bg-yellow text-black font-lato w-full justify-center sm:justify-start sm:w-1/2 flex items-center sm:pl-10 lg:pl-20 text-3xl xs:text-5xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-bold aspect-video">
+      <div className="flex flex-col sm:flex-row w-full sm:aspect-[4/3]">
+        <div className="bg-yellow text-black font-lato w-full justify-center sm:justify-start sm:w-1/2 flex items-center sm:pl-10 lg:pl-20 text-3xl xs:text-5xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-bold aspect-[4/3]">
           We lead the
           <br />
           way when it
@@ -34,7 +34,7 @@ export default function Home() {
           <br />
           and expertise.
         </div>
-        <div className="relative w-full sm:w-1/2 aspect-video">
+        <div className="relative w-full sm:w-1/2 aspect-[4/3]">
           <Image src={Picture2} alt="picture1" fill objectFit="fill" />
         </div>
       </div>
